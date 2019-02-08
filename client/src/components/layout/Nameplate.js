b29d0327dca7b35d522ac93dc7db3b278bb8d83e
Hsin Wang <hsinwang5@gmail.com>
2019-02-08 03:00:17 -0500

    finish animations and layout for hero page

--- client/src/components/layout/Nameplate.js
+++ client/src/components/layout/Nameplate.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Nameplate(props) {
   return (
-    <div className="intro__nameplate">
+    <div className="intro__nameplate animation__growNameplate">
       <div className="intro__nameplate--first">Shen</div>
       <div className="intro__nameplate--last">Wang</div>
     </div>
--- client/src/components/pages/Hero.js
+++ client/src/components/pages/Hero.js
@@ -2,25 +2,29 @@ import React, { Component } from "react";
 
 import Triangle from "../layout/Triangle";
 import Nameplate from "../layout/Nameplate";
+import EnterSubtext from "../layout/EnterSubtext";
+import Avatar from "../layout/Avatar";
 
 class Hero extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      scale1: 0,
-      scale2: 0
+      scaleTriangle1: 0,
+      scaleTriangle2: 0
     };
   }
 
   componentWillMount() {
+    //timer for triangle 1 on hero page
     setTimeout(() => {
       this.setState({
-        scale1: 1
+        scaleTriangle1: 1
       });
     }, 500);
+    //timer for triangle 2 on hero page
     setTimeout(() => {
       this.setState({
-        scale2: 1
+        scaleTriangle2: 1
       });
     }, 1500);
   }
@@ -29,15 +33,12 @@ class Hero extends Component {
     return (
       <div className="hero-screen">
         <div className="intro">
-          <Triangle top="14%" delay=".5s" scale={this.state.scale1} />
-          <Triangle top="29%" delay="1.5s" scale={this.state.scale2} />
+          <Triangle top="8%" delay=".5s" scale={this.state.scaleTriangle1} />
+          <Triangle top="20%" delay="1.5s" scale={this.state.scaleTriangle2} />
           <Nameplate />
-          <div className="intro__subtext">
-            <img src="/images/arrow.png" alt="pointy arrow" />
-            <div className="boldtext boldtext--intro">Enter</div>
-          </div>
+          <EnterSubtext />
+          <Avatar />
         </div>
-        <img class="avatar-img" src="/images/avatar.png" alt="pic of avatar" />
       </div>
     );
   }

b7a9068d4eaa9a4da999e78075b54b37f9fcf424
Hsin Wang <hsinwang5@gmail.com>
2019-02-09 23:44:22 -0500

    finish dynamic portfolio circle rendering

--- client/src/components/portfolioLayout/PortfolioLine.js
+++ client/src/components/portfolioLayout/PortfolioLine.js
@@ -6,30 +6,46 @@ class PortfolioLine extends Component {
 
     this.state = {
       height: 1,
-      top: 0
+      top: 0,
+      width: 0
     };
 
     this.myRef = React.createRef();
+    this.calculateSize = this.calculateSize.bind(this);
   }
 
   componentDidMount() {
-    const nodeHeight = this.myRef.current.offsetWidth;
-    console.log("width: " + nodeHeight);
+    this.calculateSize();
+  }
+
+  //dynamically calculates the size of portfolio circles, call on render and window resize
+  calculateSize() {
+    const width = Math.round(window.innerWidth * 0.3 * 0.69);
     this.setState(
       {
-        height: Math.round(nodeHeight * 2.115)
+        width
       },
       () => {
-        const newTop =
-          this.state.height * this.props.place - this.props.place * 5;
-        this.setState({
-          top: newTop
-        });
+        const height = Math.round(this.myRef.current.offsetWidth * 2.115);
+        this.setState(
+          {
+            height
+          },
+          () => {
+            //calculate the top offset, factoring in the size of the circle's border
+            const top =
+              this.state.height * this.props.place - this.props.place * 5;
+            this.setState({
+              top
+            });
+          }
+        );
       }
     );
   }
 
   render() {
+    console.log(this.state.width);
     let Style;
     //calculate where to place the portfolio circle along vertical axis
     //Set direction of circle + other properties in portfolio page
@@ -41,7 +57,7 @@ class PortfolioLine extends Component {
         borderRight: "0",
         height: `${this.state.height}px`,
         left: `-16%`,
-        width: `${this.props.width}px`
+        width: `${this.state.width}px`
       };
     } else {
       Style = {
@@ -51,7 +67,7 @@ class PortfolioLine extends Component {
         borderLeft: "0",
         height: `${this.state.height}px`,
         right: `-16%`,
-        width: `${this.props.width}px`
+        width: `${this.state.width}px`
       };
     }
     return (
